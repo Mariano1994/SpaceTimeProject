f8feab196f9059b3fc848830f44130c87b0833c0
Mariano <marianocapiliku@gmail.com>
2023-05-16 19:41:57 +0100

    finished: frontend environment

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,9 @@
-
 export default function Home() {
-  return ( 
+  return (
     <>
-      <h1>Hello World</h1>
+      <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+        <h1 className="text-4xl font-bold">Sua Capsula do tempo</h1>
+      </div>
     </>
   )
 }
