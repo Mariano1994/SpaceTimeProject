93330d095e21966d617398ae6cb4a9b358bdeb90
Mariano <marianocapiliku@gmail.com>
2023-05-17 13:58:33 +0100

    Finished Hero Section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
+import { User } from 'lucide-react'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
@@ -8,6 +13,68 @@ export default function Home() {
 
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* Sign in */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500 " />
+          </div>
+
+          <p className=" max-w-[140px] text-sm leading-snug">
+            {' '}
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero Section */}
+
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt=" nlw image logo" />
+
+          <div className="max-w-[420px] space-y-4">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            {' '}
+            CADASTRAR LEMBRANÇAS
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito por{' '}
+          <a
+            href="https://www.linkedin.com/in/marianocapiliku/"
+            className="font-bold underline hover:text-gray-100"
+            target="_blanck"
+            rel="noreferrer"
+          >
+            Mariano Capiliku
+          </a>{' '}
+          durante o evento NLW da{' '}
+          <a
+            href="https://rocketseat.com.br/"
+            className="font-bold underline hover:text-gray-100"
+            target="_blanck"
+            rel="noreferrer"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
       {/* Right */}
