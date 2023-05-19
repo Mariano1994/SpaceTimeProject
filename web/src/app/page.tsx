d2d81147310c269a7ac169922ae4acc6b56982d4
Mariano <marianocapiliku@gmail.com>
2023-05-19 10:30:28 +0100

    Create authentication with gitHub. Configuration of Github Oauth for the current user. This configuration will be used by the application when the user is logged in.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,12 @@
-import { User } from 'lucide-react'
-
-import nlwLogo from '../assets/nlw-spacetime-logo.svg'
-import Image from 'next/image'
+import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { HeroSection } from '@/components/HeroSection'
+import { SignInButton } from '@/components/SignInButton'
 
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      {/* Left */}
+      {/* LeftSide */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
@@ -14,80 +14,14 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        {/* Sign in */}
-        <a
-          href=""
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500 " />
-          </div>
-
-          <p className=" max-w-[140px] text-sm leading-snug">
-            {' '}
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
-
-        {/* Hero Section */}
-
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt=" nlw image logo" />
-
-          <div className="max-w-[420px] space-y-4">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-          >
-            {' '}
-            CADASTRAR LEMBRANÇAS
-          </a>
-        </div>
-
-        {/* Copyright */}
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito por{' '}
-          <a
-            href="https://www.linkedin.com/in/marianocapiliku/"
-            className="font-bold underline hover:text-gray-100"
-            target="_blanck"
-            rel="noreferrer"
-          >
-            Mariano Capiliku
-          </a>{' '}
-          durante o evento NLW da{' '}
-          <a
-            href="https://rocketseat.com.br/"
-            className="font-bold underline hover:text-gray-100"
-            target="_blanck"
-            rel="noreferrer"
-          >
-            Rocketseat
-          </a>
-        </div>
+        <SignInButton />
+        <HeroSection />
+        <Copyright />
       </div>
 
-      {/* Right */}
+      {/* RightSide */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, começa{' '}
-            <a href="" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito por{' '}
+      <a
+        href="https://www.linkedin.com/in/marianocapiliku/"
+        className="font-bold underline hover:text-gray-100"
+        target="_blanck"
+        rel="noreferrer"
+      >
+        Mariano Capiliku
+      </a>{' '}
+      durante o evento NLW da{' '}
+      <a
+        href="https://rocketseat.com.br/"
+        className="font-bold underline hover:text-gray-100"
+        target="_blanck"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
